a69b3941d814ed36c502a89f0e377474d37cbe42
ecourse <smkang@codenuri.co.kr>
2022-10-11 14:17:42 +0900

    Add files via upload

--- DAY1/5_도형편집기3.cpp
+++ DAY1/5_도형편집기3.cpp
@@ -5,21 +5,31 @@
 //        있어야 합니다.
 //        => 그래야, 기반 클래스 타입으로 묶어서 관리할때 해당 특징을 사용가능합니다.
 
+
+// 핵심 4. 기반 클래스의 멤버 함수중 파생 클래스가 재정의 하게 되는 것은
+//		  반드시 가상함수로 만들어야 합니다.
+
+
 class Shape
 {
+	int color;
 public:
-	void Draw() { std::cout << "Draw Shape" << std::endl; }
+	// 아래 2개의 멤버함수는 "virtual" 로 할까요 ? "non-virtual"로 할까요 ?
+	void SetColor(int c) { color = c; }
+	int  GetColor() const { return color; }
+
+	virtual void Draw() { std::cout << "Draw Shape" << std::endl; }
 };
 
 class Rect : public Shape
 {
 public:
-	void Draw() { std::cout << "Draw Rect" << std::endl; }
+	virtual void Draw() override { std::cout << "Draw Rect" << std::endl; }
 };
 class Circle : public Shape
 {
 public:
-	void Draw() { std::cout << "Draw Circle" << std::endl; }
+	virtual void Draw() override { std::cout << "Draw Circle" << std::endl; }
 };
 
 int main()
