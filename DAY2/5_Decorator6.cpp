e5a87e526cc56c752f3dd1245a733c78df8f1b64
ecourse <smkang@codenuri.co.kr>
2022-10-12 15:32:33 +0900

    Add files via upload

--- DAY2/5_Decorator6.cpp
+++ DAY2/5_Decorator6.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include <string>
 
-class FileStream
+// 모든 종류의 Stream 이 사용법이 동일함을 보장하기 위해
+// 인터페이스 설계
+
+struct Stream
+{
+	virtual void Write(const std::string& s) = 0;
+	virtual ~Stream() {}
+
+	// 그외에 Read(), IsOpen() 등 다양한 함수를 인터페이스로 약속
+};
+
+
+class FileStream : public Stream 
 {
 	FILE* file;
 public:
@@ -23,9 +35,11 @@ int main()
 	FileStream fs("a.txt");
 	fs.Write("hello");
 
+
 	// FileStream 외에 다양한 Stream 이 있다고 가정해 봅시다.
 	NetworkStream ns("127.0.0.1", 4000);
 	ns.Write("hello");
+
 
 	PipeStream ps("namedpipe");
 	ps.Write("hello");
