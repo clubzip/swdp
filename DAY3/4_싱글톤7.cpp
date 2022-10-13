bf6e46f4b7018791a70c9e9238028ff3e4fc2409
ecourse <smkang@codenuri.co.kr>
2022-10-13 15:28:08 +0900

    Add files via upload

--- DAY3/4_싱글톤7.cpp
+++ DAY3/4_싱글톤7.cpp
@@ -1,6 +1,12 @@
-
 #include <iostream>
 #include <mutex>
+
+// 아래 코드가 "안드로이드 소스" 에 있는 싱글톤 코드입니다.
+// github.com/aosp-mirror
+// 
+// platform system core
+// 
+// libutils/include/utils/Singleton.h 열어 보세요
 
 template<typename T>
 class Singleton
@@ -30,8 +36,6 @@ public:
 
 template<typename T> T* Singleton<T>::sinstance = nullptr; // <== 4
 template<typename T> std::mutex Singleton<T>::mtx;
-
-
 
 // Mouse 도 힙에 만드는 싱글톤으로 하고 싶습니다.
 class Mouse : public Singleton< Mouse >
