a9cfd55a7de02ba55cab060276d847b2459f8138
ecourse <smkang@codenuri.co.kr>
2022-10-13 15:14:01 +0900

    Add files via upload

--- DAY3/4_싱글톤6.cpp
+++ DAY3/4_싱글톤6.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <mutex>
 
-// RAII 
+// RAII 라는 C++ IDioms 
+// => "Resource Acquision Is Initialization" 의 약자
+// => "자원을 획득하는 것은 (자원관리객체) 가 초기화 될때(생성자)"이다.
+// 
 // 자원을 획득/반납 하는 것은 생성자/소멸자를 사용해서 해야 합니다.
 template<typename T> struct lock_guard
 {
-	T& mtx;
-	
+	T& mtx;	
 	lock_guard(T& m) : mtx(m) { mtx.lock(); }
 	~lock_guard()             { mtx.unlock(); }
 };
@@ -31,17 +33,20 @@ public:
 
 	static Cursor& getInstance()
 	{
-		lock_guard<std::mutex> g(mtx);
-//		mtx.lock();
-		if (sinstance == nullptr)
 		{
-			sinstance = new Cursor;
-		}
-//		mtx.unlock();	// <<= 자원을 반납을 이렇게 직접하지 마세요
-						// 1. 실수로 이 한줄을 생략할수 있다.
-						// 2. 이 코드의 위쪽에서 예외등의 일이 발생하면
-						//	  이 코드가 실행되지 않습니다.
+//			lock_guard<std::mutex> g(mtx);		// 생성자에서 lock 수행
+			sdt::lock_guard<std::mutex> g(mtx); // C++ 표준에 이미 있습니다.
 
+			//		mtx.lock();
+			if (sinstance == nullptr)
+			{
+				sinstance = new Cursor;
+			}
+			//		mtx.unlock();	// <<= 자원을 반납을 이렇게 직접하지 마세요
+									// 1. 실수로 이 한줄을 생략할수 있다.
+									// 2. 이 코드의 위쪽에서 예외등의 일이 발생하면
+									//	  이 코드가 실행되지 않습니다.
+		}
 		return *sinstance;
 	}
 };
