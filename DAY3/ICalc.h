1d26d38aaaaf41d821d892c5ea875d5b45b419d0
ecourse <smkang@codenuri.co.kr>
2022-10-13 13:10:35 +0900

    Add files via upload

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -1,10 +1,24 @@
 // ICalc.h
 #pragma once
 
-struct ICalc
+// Proxy 객체의 수명을 "참조계수(reference counting)" 기술로 관리한다면
+// 반드시 참조계수 관련 함수도 인터페이스에 있어야 합니다.
+
+// 참조계수용 함수는 모든 종류의 Proxy interface에 필요 합니다.
+struct IRefCount
+{
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+
+	virtual ~IRefCount() {}
+};
+
+struct ICalc : public IRefCount
 {
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
 
+// ISP : interface surrogate principle
+//       "인터페이스는 기능별로 분리해서 만들어야 한다."
