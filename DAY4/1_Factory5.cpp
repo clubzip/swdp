51483f230d32051269dfb27c46fd65c3d43bb395
ecourse <smkang@codenuri.co.kr>
2022-10-14 10:11:37 +0900

    Add files via upload

--- DAY4/1_Factory5.cpp
+++ DAY4/1_Factory5.cpp
@@ -10,6 +10,8 @@ class Shape
 public:
 	virtual void draw() = 0;
 	virtual ~Shape() {}
+
+	virtual Shape* clone() const = 0;
 };
 
 class Rect : public Shape
@@ -18,6 +20,8 @@ public:
 	void draw() override { std::cout << "draw Rect" << std::endl; }
 
 	static Shape* create() { return new Rect; }
+
+	virtual Shape* clone() const override { return new Rect(*this); }
 };
 
 class Circle : public Shape
@@ -26,6 +30,8 @@ public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
 
 	static Shape* create() { return new Circle; }
+
+	virtual Shape* clone() const override { return new Circle(*this); }
 };
 
 
@@ -33,22 +39,20 @@ class ShapeFactory
 {
 	MAKE_SINGLETON(ShapeFactory)
 
-		using CREATOR = Shape * (*)(); 
-
-	std::map<int, CREATOR> create_map;
+	std::map<int, Shape*> prototype_map;
 
 public:
-	void Register(int key, CREATOR f)
+	void Register(int key, Shape* sample)
 	{
-		create_map[key] = f;
+		prototype_map[key] = sample;
 	}
 
 	Shape* Create(int type)
 	{
 		Shape* p = nullptr;
 
-		if (create_map[type] != nullptr)
-			p = create_map[type]();
+		if (prototype_map[type] != nullptr)
+			p = prototype_map[type]->clone();
 
 		return p;
 	}
@@ -59,8 +63,23 @@ int main()
 
 	ShapeFactory& factory = ShapeFactory::getInstance();
 
-	factory.Register(1, &Rect::create);	
-	factory.Register(2, &Circle::create);
+	// 아래 코드는 공장에 "클래스"를 등록하는 코드 입니다.
+	// => 정확히는 "클래스의 객체를 만드는 함수"를 등록
+//	factory.Register(1, &Rect::create);	
+//	factory.Register(2, &Circle::create);
+
+	// 공장에 클래스가 아닌 객체를 등록해 봅시다.
+	Rect* blueRect = new Rect;  // 생성자에서 파란색으로 설정했다고 가정
+	Rect* redRect = new Rect;   // 빨간색, 크기 10 
+	Circle* redCircle = new Circle;
+
+	// 견본(prototype) 객체를 공장에 등록
+	factory.Register(1, blueRect);
+	factory.Register(2, redRect);
+	factory.Register(3, redCircle);
+
+
+
 
 
 	while (1)
