4f827a00b73ae3cfb8f237d3b79deac5257573ba
ecourse <smkang@codenuri.co.kr>
2022-10-14 09:09:58 +0900

    Add files via upload

---
+++ DAY4/1_Factory1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include "Helper.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+};
+
+// 도형을 만드는 팩토리
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory)
+public:
+	Shape* Create(int type)
+	{
+		Shape* p = nullptr;
+		if      (type == 1)	p = new Rect;
+		else if (type == 2)	p = new Circle;
+		return p;
+	}
+};
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ShapeFactory& factory = ShapeFactory::getInstance();
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		// 아래 처럼 하면 "새로운 도형 추가시" 코드 수정 필요 - OCP 만족 못함
+//		if (cmd == 1) v.push_back(new Rect);
+//		else if (cmd == 2) v.push_back(new Circle);
+		
+		// 공장을 통해서 도형 객체를 생성 합니다.
+		if (cmd > 0 && cmd < 8) // 1 ~ 7 을 도형번호로 예약
+		{
+			Shape* p = factory.Create(cmd);
+
+			if (p != nullptr)
+				v.push_back(p);
+		}
+
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw();
+			}
+		}
+	}
+}
+
+
+
+
